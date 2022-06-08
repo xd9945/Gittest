8fa31a1bf49fafbfa984653f291c7054f054a5f9
橙皮 <15919993334@163.com>
2022-06-08 17:46:27 +0800

    写了一个Demo03

---
+++ src/main/java/com/zxd/test1/Demo03.java
@@ -0,0 +1,12 @@
+package com.zxd.test1;
+
+/**
+ * @author lhj
+ * @date 2022 - 06 - 08
+ * @desc
+ */
+public class Demo03 {
+    public static void main(String[] args) {
+        System.out.println("Demo03.main");
+    }
+}

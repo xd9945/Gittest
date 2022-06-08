0c45c47ae374f2f3ca61a7170ff1ab4ac48bce6c
xd9945 <www.zxd12345@outlook.com>
2022-06-08 16:52:29 +0800

    赵四

--- src/main/java/com/zxd/test1/Hello.java
+++ src/main/java/com/zxd/test1/Hello.java
@@ -8,5 +8,9 @@ package com.zxd.test1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello,World!");
+
+        System.out.println("赵四!");
     }
+
+
 }

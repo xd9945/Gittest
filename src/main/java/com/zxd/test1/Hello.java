2b10c2819268947feeda147621152cf91157efa8
xd9945 <www.zxd12345@outlook.com>
2022-06-08 16:20:49 +0800

    第三次提交

---
+++ src/main/java/com/zxd/test1/Hello.java
@@ -0,0 +1,12 @@
+package com.zxd.test1;
+
+/**
+ * @author xudongzhou
+ * @date 2022 -06 -08
+ * @description
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
+}
